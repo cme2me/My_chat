5fd0885cdeaf11d81f783b514b0484311570e1d8
cme2me <max.zhakov@gmail.com>
2022-02-19 17:11:07 +0300

    Server is ready

--- client/src/main/java/client/Controller.java
+++ client/src/main/java/client/Controller.java
@@ -6,14 +6,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.Socket;
-import java.net.URL;
-import java.net.UnknownHostException;
-import java.util.ResourceBundle;
-
 public class Controller {
     @FXML
     public TextArea chatField;
@@ -21,11 +13,6 @@ public class Controller {
     public TextField messageField;
     @FXML
     public Button sendBtn;
-
-    private Socket socket;
-    private static final int PORT = 8189;
-    private static final String ADDRESS = "localhost";
-
 
     @FXML
     public void sendMsg(ActionEvent actionEvent) {
